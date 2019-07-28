d9c8a5274b40c393be7cb7557bf9d7d29c21fc15
huodeming <huodeming1@163.com>
2019-07-28 10:07:14 +0800

    组件中style标签lang属性和scoped属性的介绍

--- src/main/login.vue
+++ src/main/login.vue
@@ -6,6 +6,14 @@
 
 </script>
 
-<style>
 
+<style lang="scss" scoped>
+/* 
+    style标签不加scoped就是全局样式,加了就只对本组件有效 
+    普通的style标签,只支持普通的样式,如果想要启用scss 或 less,需要为style元素设置lang属性 
+*/
+    div{
+        color: blue;
+        font-style: italic;
+    }
 </style>
